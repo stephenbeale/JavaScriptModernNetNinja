cf7ad3923de4d9aadb72e469958a7d853cc8c7dc
Steve Beale <beale.stephen@gmail.com>
2024-08-06 14:52:56 +0100

    Basic calculations

--- chapter_2/sandbox.js
+++ chapter_2/sandbox.js
@@ -1,10 +1,13 @@
 let radius = 10;
 const pi = 3.142;
 
-console.log(pi * radius ** 2);
+// console.log(radius, pi);
+// console.log("Table: radius, pi");
+// console.table(radius, pi);
+// console.log("Table: [radius, pi]");
+// console.table([radius, pi]);
 
-console.log(radius, pi);
-console.log("Table: radius, pi");
-console.table(radius, pi);
-console.log("Table: [radius, pi]");
-console.table([radius, pi]);
+console.log(10 / 2);
+
+let result = radius % 3;
+console.log(result);
