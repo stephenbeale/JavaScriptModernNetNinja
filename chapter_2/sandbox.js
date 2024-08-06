0dd96edb6d28d697ce50f883f64c22ed14883dd8
Steve Beale <beale.stephen@gmail.com>
2024-08-06 14:53:56 +0100

    hmmm

--- chapter_2/sandbox.js
+++ chapter_2/sandbox.js
@@ -7,7 +7,9 @@ const pi = 3.142;
 // console.log("Table: [radius, pi]");
 // console.table([radius, pi]);
 
-console.log(10 / 2);
+// console.log(10 / 2);
+// let result = radius % 3;
+// console.log(result);
 
-let result = radius % 3;
+let result = pi * radius ** 2;
 console.log(result);
