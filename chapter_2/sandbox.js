904958953d30e28237725b9316731e38e9dc0c78
Steve Beale <beale.stephen@gmail.com>
2024-08-06 14:51:35 +0100

    Console logging calculations

---
+++ chapter_2/sandbox.js
@@ -0,0 +1,10 @@
+let radius = 10;
+const pi = 3.142;
+
+console.log(pi * radius ** 2);
+
+console.log(radius, pi);
+console.log("Table: radius, pi");
+console.table(radius, pi);
+console.log("Table: [radius, pi]");
+console.table([radius, pi]);
