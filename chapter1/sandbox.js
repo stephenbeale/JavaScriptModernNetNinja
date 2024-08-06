67e69eb45daa67d508803a46ee9130e2cedb16f2
Steve Beale <beale.stephen@gmail.com>
2024-08-06 11:22:04 +0100

    Concatenation

--- chapter1/sandbox.js
+++ chapter1/sandbox.js
@@ -3,3 +3,9 @@ console.log("hello world");
 
 let email = "mario@thenetninja.co.uk";
 console.log(email);
+
+let firstName = "Brandon";
+let lastName = "Sanderson";
+
+let fullName = firstName + " " + lastName;
+console.log(fullName);
