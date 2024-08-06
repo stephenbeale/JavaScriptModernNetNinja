37500c8b4a47b9061fe86c225dad2f82f15cf16e
Steve Beale <beale.stephen@gmail.com>
2024-08-06 12:54:16 +0100

    Off piste but great playing with console log

---
+++ chapter_2/sandbox.js
@@ -0,0 +1,29 @@
+//common string methods
+
+let email = "mario@thenetninja.co.uk";
+let result = email.lastIndexOf("n");
+console.log("hello");
+console.log(result);
+
+console.warn("hello");
+
+console.group("hello");
+console.assert(false, "hello");
+
+console.table("hello");
+
+console.groupCollapsed("hello");
+
+console.count(email.substring(0, 3));
+console.count(email.substring(0, 3));
+console.count("@");
+console.count("e");
+
+console.time();
+console.timeStamp();
+
+console.timeLog();
+console.timeEnd();
+
+//Traces the call stack
+console.trace("tracing");
