12b0ab4f6d797c5bccc87461827fffeb517a40c5
Steve Beale <beale.stephen@gmail.com>
2024-08-07 16:12:19 +0100

    Always use STRICT COMPARISON i.e. triple equals in JS

--- chapter_2/sandbox.js
+++ chapter_2/sandbox.js
@@ -1,9 +1,15 @@
 let age = 25;
 //loose comparison - differenty types can still be equal
 
-console.log(age == 25);
-console.log(age == "25");
+// console.log(age == 25);
+// console.log(age == "25");
 
-console.log(age != 25);
-console.log(age != "25");
+// console.log(age != 25);
+// console.log(age != "25");
+
 //strict comparison - differenty types cannot be equal
+
+console.log("Strict comparison, using ===");
+console.log(age === 25);
+console.log("Strict comparison, using === for a string '25'");
+console.log(age === "25");
