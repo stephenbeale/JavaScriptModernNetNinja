fe0064ef45c38c9dc5933f79a3094e6d9b96bf35
Steve Beale <beale.stephen@gmail.com>
2024-08-07 14:55:09 +0100

    Not operators with booleans

--- chapter_2/sandbox.js
+++ chapter_2/sandbox.js
@@ -3,20 +3,27 @@ console.log(true, false, "true", "false");
 
 //methods can return booleans
 let email = "luigi@thenetninja.co.uk";
-console.log("Result includes @");
-let result = email.includes("@");
-console.log(result);
+// console.log("Result includes @");
+// let result = email.includes("@");
+// console.log(result);
 
-console.log("Result includes !");
-result = email.includes("!");
-console.log(result);
+// console.log("Result includes !");
+// result = email.includes("!");
+// console.log(result);
 
-console.log("Names array");
+// console.log("Names array");
 let names = ["mario", "luigi", "toad"];
-console.log(names);
+// console.log(names);
 
-console.log("Names array includes i");
-names = email.includes("i");
-console.log(names);
+// console.log("Names array includes i");
+// names = email.includes("i");
+// console.log(names);
 
 //comparison operators
+let age = 25;
+console.log("Is age == 25?");
+console.log(age == 25);
+console.log("Is age == 35?");
+console.log(age == 35);
+console.log("Is age != 35?");
+console.log(age != 35);
