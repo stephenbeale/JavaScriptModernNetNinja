16d4d7ba50cdb56c7e7a22c208d7be26b5900a97
Steve Beale <beale.stephen@gmail.com>
2024-08-07 09:09:47 +0100

    Mixed types array - bad idea but possible

--- chapter_2/sandbox.js
+++ chapter_2/sandbox.js
@@ -1,18 +1,21 @@
 let ninjas = ["yoshi", "mario", "luigi"];
-console.log(ninjas);
+// console.log(ninjas);
 
-console.log(ninjas[1]);
+// console.log(ninjas[1]);
 
-ninjas[1] = "ryu";
-console.log(ninjas[1]);
+// ninjas[1] = "ryu";
+// console.log(ninjas[1]);
 
-console.log("Ages array");
-let ages = ["20", "25", "30", "35"];
+// console.log("Ages array");
+// let ages = ["20", "25", "30", "35"];
 
-console.log(ages);
-console.log("Ages array as table");
-console.table(ages);
+// console.log(ages);
+// console.log("Ages array as table");
+// console.table(ages);
 
-console.log("Ninjas and ages arrays as group collapsed");
-console.groupCollapsed(ninjas, ages);
-console.groupEnd();
+// console.log("Ninjas and ages arrays as group collapsed");
+// console.groupCollapsed(ninjas, ages);
+// console.groupEnd();
+
+let random = ["shaun", "crystal", 30, 20];
+console.log(random);
