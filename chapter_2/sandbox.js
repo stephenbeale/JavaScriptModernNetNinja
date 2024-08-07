979b43ba0b9ec69a446f1cf872af0052bd76a1b3
Steve Beale <beale.stephen@gmail.com>
2024-08-07 12:59:17 +0100

    undefined example

--- chapter_2/sandbox.js
+++ chapter_2/sandbox.js
@@ -1,27 +1,4 @@
-//template strings
-const title = "Best reads of 2024";
-const author = "Mario";
-const likes = 30;
+let age;
 
-//concatenation way
-let result =
-  "The blog called " + title + " by " + author + " has " + likes + " likes";
-console.log(result);
-
-//template string way
-
-//template string AKA template literal
-let tresult = `The blog called ${title} by ${author} has ${likes} likes`;
-console.table([result]);
-//creating html templates
-
-let html = `
-<h2>${title}</h2>
-<p>By ${author}</p>
-<span>This blog has ${likes} likes</span>
-`;
-console.log(html);
-
-console.table([html]);
-
-document.body.innerHTML = html;
+console.log(age, age + 3, `the age is ${age}`);
+ 
