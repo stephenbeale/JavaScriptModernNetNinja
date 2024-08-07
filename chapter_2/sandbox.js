3943ace4c49d7dd54c6c3c769c55ddb35c064f72
Steve Beale <beale.stephen@gmail.com>
2024-08-07 09:08:20 +0100

    Arrays and various console outputs

--- chapter_2/sandbox.js
+++ chapter_2/sandbox.js
@@ -5,3 +5,14 @@ console.log(ninjas[1]);
 
 ninjas[1] = "ryu";
 console.log(ninjas[1]);
+
+console.log("Ages array");
+let ages = ["20", "25", "30", "35"];
+
+console.log(ages);
+console.log("Ages array as table");
+console.table(ages);
+
+console.log("Ninjas and ages arrays as group collapsed");
+console.groupCollapsed(ninjas, ages);
+console.groupEnd();
