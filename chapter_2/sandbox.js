f50da47bebca5b6ec09a22c1a9c36bb976d18bd5
Steve Beale <beale.stephen@gmail.com>
2024-08-07 14:44:04 +0100

    Includes true or false

--- chapter_2/sandbox.js
+++ chapter_2/sandbox.js
@@ -3,7 +3,12 @@ console.log(true, false, "true", "false");
 
 //methods can return booleans
 let email = "luigi@thenetninja.co.uk";
+console.log("Result includes @");
 let result = email.includes("@");
+console.log(result);
+
+console.log("Result includes !");
+result = email.includes("!");
 console.log(result);
 
 //comparison operators
