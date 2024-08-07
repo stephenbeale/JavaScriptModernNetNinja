aebb7716c5fc0c6625fa90542f8b3f2dc8a36826
Steve Beale <beale.stephen@gmail.com>
2024-08-07 16:21:29 +0100

    first commit message through the terminal

--- chapter_2/sandbox.js
+++ chapter_2/sandbox.js
@@ -1,20 +1,3 @@
-let age = 25;
-//loose comparison - differenty types can still be equal
-
-// console.log(age == 25);
-// console.log(age == "25");
-
-// console.log(age != 25);
-// console.log(age != "25");
-
-//strict comparison - differenty types cannot be equal
-
-console.log("Strict comparison, using ===");
-console.log(age === 25);
-console.log("Strict comparison, using === for a string '25'");
-console.log(age === "25");
-
-console.log("Strict comparison, using !== for '25'");
-console.log(age !== 25);
-console.log("Strict comparison, using !== for a string '25'");
-console.log(age !== "25");
+//type conversion
+let score = 100;
+console.log(score + 1);
