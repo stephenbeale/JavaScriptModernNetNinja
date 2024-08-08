2bf44b33ac83b87edce6fb57412d77d4db2d3f52
Steve Beale <s.j.beale@swansea.ac.uk>
2024-08-08 11:28:18 +0100

    While loops practice

--- chapter_2/sandbox.js
+++ chapter_2/sandbox.js
@@ -1,9 +1,17 @@
 //while loops
 
+const names = ["luigi", "mario", "yoshi"];
+let i = 0;
+
 //infinite loop if true! Don't do this
 
-let i = 0;
-while (i < 5) {
-    console.log("in loop", i);
+// let i = 0;
+// while (i < 5) {
+//     console.log("in loop", i);
+//     i++;
+// };
+
+while (i < names.length) {
+    console.log(names[i]);
     i++;
-};
+}
