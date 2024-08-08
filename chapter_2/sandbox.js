6016ee48dd225ed1c9852b2d1d364585a0dae918
Steve Beale <s.j.beale@swansea.ac.uk>
2024-08-08 11:16:13 +0100

    For loops and printing html

--- chapter_2/sandbox.js
+++ chapter_2/sandbox.js
@@ -1,39 +1,15 @@
-//type conversion
-let score = "100";
+//for loops
 
-//type conversion
-score = Number(score);
+// for (let i = 0; i < 5; i++) {
+//     console.log('in loop:', i)
+// }
+// console.log('loop finished')
 
-console.log(score + 1);
+const names = ["shaun", "mario", "luigi"];
 
-console.log("Type checking e.g. typeof score outputs: ");
-console.log(typeof score);
-
-//cast to string
-let result = String(50);
-
-console.log("Type checking e.g. 'result, typeof result' outputs: ");
-console.log(result, typeof result);
-
-//Boolean - truthy or falsey values
-console.log("Booleans");
-result = Boolean(100);
-console.log(result, typeof result);
-
-//Zero is false
-result = Boolean(0);
-console.log(result, typeof result);
-
-//Minus numbers are true!
-result = Boolean(-100);
-console.log(result, typeof result);
-
-//String of zero - is true!
-//strings are always truthy
-result = Boolean("0");
-console.log(result, typeof result);
-
-//Empty string are always false!
-//strings are always truthy
-result = Boolean("");
-console.log(result, typeof result);
+for (let i = 0; i < names.length; i++) {    
+    // console.log(names[i])
+    let html = 
+    `<div>${names[i]}</div>`;
+    console.log(html);
+}
