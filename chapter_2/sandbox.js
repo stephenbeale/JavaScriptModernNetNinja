843899ee31e583d23747c676f74f2514126e0f62
Steve Beale <s.j.beale@swansea.ac.uk>
2024-08-08 14:03:11 +0100

    Continues at zero now without logging

--- chapter_2/sandbox.js
+++ chapter_2/sandbox.js
@@ -1,9 +1,14 @@
 //break and continue
 
-const scores = [50, 30, 24, 78, 68, 100];
+const scores = [50, 30, 24, 0, 78, 0, 0, 68, 100];
 
 for (let i = 0; i < scores.length; i++) {
-    console.log("your score is: " + scores[i]);
+    if (scores[i] === 0) {
+     //ignore rest of code inside block but still goes through for each
+        continue;
+    }   
+    
+    console.log("your score is: " + scores[i]);   
     
     if (scores[i] === 100) {
         console.log("congrats!");
