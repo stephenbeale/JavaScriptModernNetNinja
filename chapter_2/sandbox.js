5b72b429b1c1eea8247e7cdef8f8aeee6759e49e
Steve Beale <s.j.beale@swansea.ac.uk>
2024-08-08 13:51:24 +0100

    Breaks now

--- chapter_2/sandbox.js
+++ chapter_2/sandbox.js
@@ -3,8 +3,11 @@
 const scores = [50, 30, 24, 78, 68, 100];
 
 for (let i = 0; i < scores.length; i++) {
-    if (scores[i] < 60) {
-        continue;
+    console.log("your score is: " + scores[i]);
+    
+    if (scores[i] === 100) {
+        console.log("congrats!");
+        break;        
     }
     console.log(`Score: ${scores[i]}`);
 }
