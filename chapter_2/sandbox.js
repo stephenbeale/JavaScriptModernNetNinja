9b21262e9ca5f853ef870b8ad02919611c06fcd5
Steve Beale <beale.stephen@gmail.com>
2024-08-09 10:12:59 +0100

    Block level scope

--- chapter_2/sandbox.js
+++ chapter_2/sandbox.js
@@ -1,10 +1,8 @@
 //variables and block scope
+
+let age = 30;
+
 if (true) {
-  var a = 1;
-  let b = 2;
-  const c = 3;
-  console.log(a);
-  console.log(b);
-  console.log(c);
+  console.log(age);
 }
-console.log(a);
+console.log(age);
