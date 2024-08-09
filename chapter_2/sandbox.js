f2993bb47fa3424eaf111ef13dbb8bff01a09168
Steve Beale <beale.stephen@gmail.com>
2024-08-09 10:17:34 +0100

    Variable with local scope - dangerous

--- chapter_2/sandbox.js
+++ chapter_2/sandbox.js
@@ -4,7 +4,8 @@ let age = 30;
 
 age = 50;
 if (true) {
-  age = 40;
+  //recreates the variable with local scope only
+  let age = 40;
   console.log("inside code block", age);
 }
 console.log("outside code block", age);
