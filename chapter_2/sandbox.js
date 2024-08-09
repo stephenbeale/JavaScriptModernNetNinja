903d5836ea781fe6058c4c2aaa8e4292568ee21e
Steve Beale <beale.stephen@gmail.com>
2024-08-09 09:49:52 +0100

    End of switch statements

--- chapter_2/sandbox.js
+++ chapter_2/sandbox.js
@@ -1,19 +1,26 @@
 //switch statements
 
-const grade  = "D";
+const grade = "D";
 
 switch (grade) {
-    case "A": console.log("you got an A");
+  case "A":
+    console.log("you got an A");
     break;
-    case "B": console.log("you got a B");
+  case "B":
+    console.log("you got a B");
     break;
-    case "C": console.log("you got a C");
+  case "C":
+    console.log("you got a C");
     break;
-    case "D": console.log("you got a D");
+  case "D":
+    console.log("you got a D");
     break;
-    case "E": console.log("you got a E");
+  case "E":
+    console.log("you got a E");
     break;
-    case "F": console.log("you got an F");
+  case "F":
+    console.log("you got an F");
     break;
-    default: console.log("not a valid grade");
+  default:
+    console.log("not a valid grade");
 }
