8965cecdcda40982d85995765598e57edf858b6d
Steve Beale <s.j.beale@swansea.ac.uk>
2024-09-03 15:48:25 +0100

    Close to end of day

--- chapter_2/sandbox.js
+++ chapter_2/sandbox.js
@@ -1,26 +1,6 @@
-//switch statements
-
-const grade = "D";
-
-switch (grade) {
-  case "A":
-    console.log("you got an A");
-    break;
-  case "B":
-    console.log("you got a B");
-    break;
-  case "C":
-    console.log("you got a C");
-    break;
-  case "D":
-    console.log("you got a D");
-    break;
-  case "E":
-    console.log("you got a E");
-    break;
-  case "F":
-    console.log("you got an F");
-    break;
-  default:
-    console.log("not a valid grade");
+// functional declaration
+function greet() {
+    console.log("hello world");
 }
+
+// functional expression
