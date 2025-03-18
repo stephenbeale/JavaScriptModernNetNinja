c3815990c99f1cb2ff11b7d51ace7493de12a01b
Steve Beale <steve@imagitech.co.uk>
2025-03-18 08:54:26 +0000

    Arrow function alongside regular function

--- chapter_2/sandbox.js
+++ chapter_2/sandbox.js
@@ -1,8 +1,14 @@
+
+//regular function
 const calcArea = function(radius) {
     return 3.142 * radius **2;
 }
 
 // arrow function
+const calcArea = (radius) => {
+    return 3.142 * radius **2;
+}
+
 
 const area = calcArea(5);
 console.log('area is:', area);
