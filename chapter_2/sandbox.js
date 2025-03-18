d1b228c0ada3ebeec11c71d02b5dfde9736edcd7
Steve Beale <steve@imagitech.co.uk>
2025-03-18 09:07:13 +0000

    Remember that the code runs in the console on the URI under the dir name e.g. /chapter_2

--- chapter_2/sandbox.js
+++ chapter_2/sandbox.js
@@ -1,8 +1,8 @@
 
 //regular function
-const calcArea = function(radius) {
-    return 3.142 * radius **2;
-}
+// const calcArea = function(radius) {
+//     return 3.142 * radius **2;
+// }
 
 // arrow function
 const calcArea = (radius) => {
