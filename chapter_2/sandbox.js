c8a64c3982320c33cb2c47a45030c6ed9b46fe4a
Steve Beale <steve@imagitech.co.uk>
2025-03-22 18:16:24 +0000

    Basic for-each example with index as well.

--- chapter_2/sandbox.js
+++ chapter_2/sandbox.js
@@ -3,6 +3,6 @@
 let people = ['mario', 'luigi', 'yoshi'];
 
 //Call the method on the people array, person name is arbitray, created on the fly
-people.forEach(person => {
-    console.log(person);
+people.forEach((person, index) => {
+    console.log(index, person);
 });
