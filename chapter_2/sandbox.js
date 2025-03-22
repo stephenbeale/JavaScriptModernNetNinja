bd5dd15c1026f3f1666055e21204d60d4b3c46cf
Steve Beale <steve@imagitech.co.uk>
2025-03-22 18:06:05 +0000

    Setting up for video 35.

--- chapter_2/sandbox.js
+++ chapter_2/sandbox.js
@@ -1,40 +1,11 @@
-// practise arrow functions
-//Convert this regular function to an arrow function
-// const greet = function () {
-//     return 'hello, world';
-//   };
+//callbacks and for-each
 
-//But can also remove certain parts
-//   const greet = () => {
-//     return 'hello, world';
-//   }
+const myFunc = (callbackFunc) => {
+    //do something
+    let value = 50;
+    callbackFunc(value);
+}
 
-// //Most minimal arrow function with no parameters
-//   const greet = () => 'hello world';
-
-
-//   const result = greet();
-
-//   console.log(result);
-  
-//   const bill = function(products, tax) {
-//     let total = 0;
-//     for (let i = 0; i < products.length; i++) {
-//       total += products[i] + products[i] * tax;
-//     }
-//     return total;
-//   };
-
-
-//Too much logic to put it into one line, but arrow version keeps it as small as possible
-//Just remove function keyword, add arrow instead.
-  const bill = (products, tax) => {
-    let total = 0;
-    for (let i = 0; i < products.length; i++) {
-        total += products[i] + products[i] * tax;
-      }
-      return total;
-  }
-
-console.log(bill([1, 4], 0.2));
-  
+myFunc(function(value){
+    console.log(value);
+});
