230cb2936b1dac1def16923bc65e9fb34d18d509
Steve Beale <steve@imagitech.co.uk>
2025-03-22 19:07:19 +0000

    Basic object creation example.

---
+++ chapter_2/sandbox.js
@@ -0,0 +1,9 @@
+//object literals
+
+let user = {
+    name: 'crystal',
+    age: '30',
+    email: 'crystal@thenetninja.co.uk',
+    location: 'berlin',
+    blogs: ['why mac and cheese rules', '10 things to make after marmite']
+};
