7e269c7d090fe3702b961852ad47ba2226ee7f7a
Steve Beale <steve@imagitech.co.uk>
2025-03-26 06:13:40 +0000

    Modifying values on the heap, bnot the stack, for demo. Reference types e.g. objects on heap, primitives on stack.

--- chapter_5/sandbox.js
+++ chapter_5/sandbox.js
@@ -1,14 +1,27 @@
 //primitive values
 
-let scoreOne = 50;
-let scoreTwo = scoreOne;
+// // let scoreOne = 50;
+// // let scoreTwo = scoreOne;
 
-console.log('scoreOne: ${scoreOne}', 'scoreTwo: ${scoreTwo}');
+// // console.log(`scoreOne: ${scoreOne}`, `scoreTwo: ${scoreTwo}`);
 
-scoreOne = 100;
+// // scoreOne = 100;
 
-console.log('scoreOne: ${scoreOne}', 'scoreTwo: ${scoreTwo}');
+// // console.log(`scoreOne: ${scoreOne}`, `scoreTwo: ${scoreTwo}`);
 
 //reference values
 
-// userOne =  {name: 'ryu'}
+const userOne =  {name: 'ryu', age: 30};
+
+//copies pointer on the stack, not the object on the heap
+const userTwo = userOne;
+
+console.log(userOne, userTwo);
+
+//only stored on the heap, not the stack, 
+// so changing userOne also changes userTwo given the pointer connection
+userOne.age = 40;
+console.log(userOne, userTwo);
+
+userOne.name = "chun-li";
+console.log(userOne, userTwo);
