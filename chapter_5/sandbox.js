3857be4f6bfc33cd66b8ee91a6699f668a2eb44d
Steve Beale <steve@imagitech.co.uk>
2025-03-26 05:56:06 +0000

    Also adding random.

--- chapter_5/sandbox.js
+++ chapter_5/sandbox.js
@@ -10,4 +10,13 @@ const area = 7.7;
 
 console.log(Math.round(area));
 console.log(Math.floor(area));
+console.log(Math.ceil(area));
+console.log(Math.trunc(area));
 
+
+// random numbers
+
+const random = Math.random();
+
+console.log(random);
+console.log(Math.round(random));
