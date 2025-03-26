bc934247c67a2dbacfcde244076788d71b87d60b
Steve Beale <steve@imagitech.co.uk>
2025-03-26 07:01:36 +0000

    Copy element to get unique selector.

--- chapter_6/sandbox.js
+++ chapter_6/sandbox.js
@@ -11,3 +11,13 @@ const error = document.querySelector('.error');
 //Shows a reference to the p tag. First step to manipulating it.
 console.log(error);
 
+//Looks for a more specific error, div with a class of 'error'
+const divError = document.querySelector('div.error');
+
+//Shows a reference to the p tag. First step to manipulating it.
+//Needed when you have multiple of the same thing.
+console.log(divError);
+
+//to get unique selector, right click > copy > Copy selector - gives unique selector
+//e.g. head > title
+console.log(document.querySelector('head > title'));
