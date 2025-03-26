790bdfa3378584cd7dfea1797074e626827be831
Steve Beale <steve@imagitech.co.uk>
2025-03-26 05:53:01 +0000

    Some math functions.

--- chapter_5/sandbox.js
+++ chapter_5/sandbox.js
@@ -1,49 +1,13 @@
+//Math object
 
-//object literals
-const blogs = [
-    { 
-        title: 'why mac & cheese rules', 
-        likes: 30 
-    },
-    {
-        title: '10 things to make with marmite',
-        likes: 50
-    }
-]
+console.log(Math);
+console.log(Math.PI);
+console.log(Math.E);
 
-//console.log(blogs)
+//Math methods
 
+const area = 7.7;
 
-let user = {
-    name: 'crystal',
-    age: '30',
-    email: 'crystal@thenetninja.co.uk',
-    location: 'berlin',
-    blogs: [    { 
-            title: 'why mac & cheese rules', 
-            likes: 30 
-        },
-        {
-            title: '10 things to make with marmite',
-            likes: 50
-        }
-    ],
-    login() {
-        console.log('the user logged in');
-    },
-    logout() {
-        console.log('the user is logged out');
-    },
-    logBlogs() {
-        console.log('this user has written the following blogs:');
-        this.blogs.forEach(blog => {
-            console.log(blog.title, blog.likes);
-        })
-    }
+console.log(Math.round(area));
+console.log(Math.floor(area));
 
-};
-
-//this used in this way logs the global context in JS, which is the 'window' object, as can be seen on the console.
-//console.log(this);
-user.logBlogs();
-//
