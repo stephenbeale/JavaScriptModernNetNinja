28f5a2d76de9adca3926548e9f074979fb4f0376
Steve Beale <steve@imagitech.co.uk>
2025-03-26 06:51:14 +0000

    Query selector example with a single p tag.

---
+++ chapter_6/sandbox.js
@@ -0,0 +1,5 @@
+//grabs the first p tag in the DOM, none of the others
+const para = document.querySelector('p');
+
+//Shows a reference to the p tag. First step to manipulating it.
+console.log(para);
