ae4fccedf68b31e8735de4a7de801ee817d4928d
Steve Beale <steve@imagitech.co.uk>
2025-03-26 06:55:23 +0000

    Query selector example with a error class, too.

--- chapter_6/sandbox.js
+++ chapter_6/sandbox.js
@@ -3,3 +3,10 @@ const para = document.querySelector('p');
 
 //Shows a reference to the p tag. First step to manipulating it.
 console.log(para);
+
+//grabs the first class of error in the DOM, none of the others
+const error = document.querySelector('.error');
+
+//Shows a reference to the p tag. First step to manipulating it.
+console.log(error);
+
