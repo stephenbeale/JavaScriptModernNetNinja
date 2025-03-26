5b8da7b3ffc92f252389694c61b02567514c6c56
Steve Beale <steve@imagitech.co.uk>
2025-03-26 06:56:46 +0000

    Class selector note.

--- chapter_6/sandbox.js
+++ chapter_6/sandbox.js
@@ -5,6 +5,7 @@ const para = document.querySelector('p');
 console.log(para);
 
 //grabs the first class of error in the DOM, none of the others
+// needs the . to make it look for a css class, given the syntax
 const error = document.querySelector('.error');
 
 //Shows a reference to the p tag. First step to manipulating it.
